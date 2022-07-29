97bc9638f092c49dc29969122fe67975de3bae1b
devELIOper <lopeselio@gmail.com>
2022-07-29 09:42:01 +0530

    added contracts migrations packages

---
+++ packages/protocol/truffle-config.js
@@ -0,0 +1,71 @@
+const ContractKit = require('@celo/contractkit')
+const Web3 = require('web3')
+const path = require('path')
+
+// Connect to the desired network
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+// const kit = ContractKit.newKitFromWeb3(web3)
+const kit = ContractKit.newKitFromWeb3(web3)
+// const kit = Kit.newKit('https://forno.celo.org') // mainnet endpoint
+
+const getAccount = require('./utils/getAccount').getAccount
+
+async function awaitWrapper(){
+    let account = await getAccount()
+    console.log(`Account address: ${account.address}`)
+    kit.addAccount(account.privateKey)
+    console.log(kit.connection.web3.currentProvider)
+}
+
+awaitWrapper()
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+
+  // The following line will put the compiled contracts and associated info at ./client/contracts
+  contracts_build_directory: path.join(__dirname, "src/abis"),
+
+  networks: {
+    // Use the development network if you are using @celo/ganache-cli
+    // https://www.npmjs.com/package/@celo/ganache-cli
+    development: {
+     host: "127.0.0.1",
+     port: 8545,
+     network_id: "*",
+    },
+    alfajores: {
+      networkCheckTimeout: 100000,
+      provider: kit.connection.web3.currentProvider,
+      network_id: 44787,
+      gas: 4000000
+    },
+    mainnet: {
+      provider: kit.web3.currentProvider,
+      network_id: 42220
+    }
+    
+  },
+  compilers: {
+    solc: {
+      version: "0.5.17",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+  mocha: {
+    bail: true,
+    enableTimeouts: false,
+  },
+  plugins: [
+    'truffle-plugin-verify',
+    'solidity-coverage'
+  ],
+  kit,
+  web3
+
+};
---
+++ packages/protocol/utils/getAccount.js
@@ -0,0 +1,38 @@
+/*
+    This script looks for a .secret file in the project root. 
+    If none is found, it creates an account private key and saves the key in a new .secret file. 
+    If a .secret file is found, it returns an account object corresponding to the stored private key.
+*/
+
+const Web3 = require('web3')
+const fs = require('fs')
+const path = require('path')
+var web3 = new Web3();
+
+const filePath = path.join(__dirname, '../.secret');
+console.log(filePath)
+
+function getAccount() {
+    return new Promise(resolve => {
+        if(fs.existsSync(filePath)){
+            fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+                resolve(web3.eth.accounts.privateKeyToAccount(data))
+            })
+        } else {
+            let randomAccount = web3.eth.accounts.create()
+        
+            fs.writeFile(filePath, randomAccount.privateKey, (err) => {
+                if(err) {
+                    return console.log(err);
+                }
+            })
+
+            resolve(randomAccount)
+        }
+    })
+}
+
+
+module.exports = {
+    getAccount
+}
